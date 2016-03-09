221fa144a51514c145940ff4f832f2a1509f33dd
Steve McCarthy <steve@redlua.com>
2016-03-09 00:48:59 -0500

    Implement all bucket actions, refactor impending

--- bucket.go
+++ bucket.go
@@ -79,13 +79,142 @@ func (b *B2) ListBuckets() ([]Bucket, error) {
 }
 
 func (b *B2) CreateBucket(name string, bType BucketType) (*Bucket, error) {
-	return nil, nil
+	reqBody, err := json.Marshal(bucketRequest{
+		AccountID:  b.AccountID,
+		BucketName: name,
+		BucketType: bType,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST",
+		b.MakeApiUrl("/b2api/v1/b2_create_bucket"), bytes.NewReader(reqBody))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", b.AuthorizationToken)
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode == 200 {
+		bucket := &Bucket{}
+		if err := json.Unmarshal(body, bucket); err != nil {
+			return nil, err
+		}
+
+		bucket.B2 = b
+
+		return bucket, nil
+	} else {
+		errJson := errorResponse{}
+		if err := json.Unmarshal(body, &errJson); err != nil {
+			return nil, err
+		}
+
+		return nil, errJson
+	}
 }
 
-func (b *Bucket) Update(bType BucketType) (*Bucket, error) {
-	return nil, nil
+func (b *Bucket) Update(newBucketType BucketType) error {
+	reqBody, err := json.Marshal(bucketRequest{
+		AccountID:  b.B2.AccountID,
+		BucketID:   b.BucketID,
+		BucketType: newBucketType,
+	})
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST",
+		b.B2.MakeApiUrl("/b2api/v1/b2_update_bucket"), bytes.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", b.B2.AuthorizationToken)
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode == 200 {
+		bucket := &Bucket{}
+		if err := json.Unmarshal(body, bucket); err != nil {
+			return err
+		}
+
+		b.BucketType = bucket.BucketType
+
+		return nil
+	} else {
+		errJson := errorResponse{}
+		if err := json.Unmarshal(body, &errJson); err != nil {
+			return err
+		}
+
+		return errJson
+	}
 }
 
 func (b *Bucket) Delete() error {
-	return nil
+	reqBody, err := json.Marshal(bucketRequest{
+		AccountID: b.B2.AccountID,
+		BucketID:  b.BucketID,
+	})
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST",
+		b.B2.MakeApiUrl("/b2api/v1/b2_delete_bucket"), bytes.NewReader(reqBody))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", b.B2.AuthorizationToken)
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode == 200 {
+		bucket := &Bucket{}
+		if err := json.Unmarshal(body, bucket); err != nil {
+			return err
+		}
+
+		return nil
+	} else {
+		errJson := errorResponse{}
+		if err := json.Unmarshal(body, &errJson); err != nil {
+			return err
+		}
+
+		return errJson
+	}
 }
