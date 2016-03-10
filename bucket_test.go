6f3df65581fb015042b24f2746ff84547df4e4b6
Steve McCarthy <steve@redlua.com>
2016-03-10 13:47:01 -0500

    Add errors tests for all bucket operations

--- bucket_test.go
+++ bucket_test.go
@@ -51,3 +51,74 @@ func Test_B2_ListBuckets_Errors(t *testing.T) {
 		s.Close()
 	}
 }
+
+func Test_B2_CreateBucket_Success(t *testing.T) {
+	t.Skip()
+}
+
+func Test_B2_CreateBucket_Errors(t *testing.T) {
+	codes, bodies := errorResponses()
+	b := makeTestB2()
+
+	for i := range codes {
+		s := setupRequest(codes[i], bodies[i])
+
+		bucket, err := b.CreateBucket("bucket", AllPrivate)
+		testErrorResponse(err, codes[i], t)
+		if bucket != nil {
+			t.Errorf("Expected bucket to be empty, instead got %+v", bucket)
+		}
+
+		s.Close()
+	}
+}
+
+func Test_Bucket_Update_Success(t *testing.T) {
+	t.Skip()
+}
+
+func Test_Bucket_Update_Errors(t *testing.T) {
+	codes, bodies := errorResponses()
+	b := makeTestB2()
+	bucket := makeTestBucket(b)
+
+	for i := range codes {
+		s := setupRequest(codes[i], bodies[i])
+
+		err := bucket.Update(AllPublic)
+		testErrorResponse(err, codes[i], t)
+		if bucket.BucketType != AllPrivate {
+			t.Errorf("Expected bucket type to be private, instead got %+v", bucket.BucketType)
+		}
+
+		s.Close()
+	}
+}
+
+func Test_Bucket_Delete_Success(t *testing.T) {
+	t.Skip()
+}
+
+func Test_Bucket_Delete_Errors(t *testing.T) {
+	codes, bodies := errorResponses()
+	b := makeTestB2()
+	bucket := makeTestBucket(b)
+
+	for i := range codes {
+		s := setupRequest(codes[i], bodies[i])
+
+		err := bucket.Update(AllPublic)
+		testErrorResponse(err, codes[i], t)
+
+		s.Close()
+	}
+}
+
+func makeTestBucket(b *B2) *Bucket {
+	return &Bucket{
+		BucketID:   "1",
+		BucketName: "name",
+		BucketType: AllPrivate,
+		B2:         b,
+	}
+}
